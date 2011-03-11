1d451b7351e2ee65c4b47658997ab2cfb66619b9
thiagobrandam <thiago@thiago.redealumni>
2011-03-11 18:47:39 -0300

    Added config.ru

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-require 'rubygems'
-require 'bundler'
-
-Bundler.require
-
 post "/sendgrid_event.json" do
   request.body.rewind
   data = JSON.parse(request.body.read)
   data.inspect
 end
+
+# para testar:
+# curl -H 'Content-Type: application/json' -d "{\"val\":\"var\"}" -X POST http://localhost:4567/sendgrid_event.json > error.html
