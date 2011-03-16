f63b20ab4044dd8ac755dbb809ad004365e6aafd
thiagobrandam <thiago@thiago.redealumni>
2011-03-16 15:51:44 -0300

    Added DataMapper model to represent a sendgrid event

--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 require 'bundler_helper'
 require 'sinatra'
+require 'db/setup'
 require 'app'
 
 use SendgridEventProxy::Application
---
+++ db/setup.rb
@@ -0,0 +1,21 @@
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class SendgridEvent
+  
+  include DataMapper::Resource
+  
+  property :id,       Serial
+  property :event,    String
+  property :email,    String
+  property :category, String
+  property :reason,   String
+  property :response, String
+  property :attempt,  String
+  property :reason,   String
+  property :type,     String
+  property :status,   String
+  property :url,      String
+  
+end
+
+DataMapper.auto_upgrade!
