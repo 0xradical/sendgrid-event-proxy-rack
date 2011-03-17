defc0d537e86d5d17ae402ef6c69e8ba620c8a86
thiagobrandam <thiago@thiagobrandam.local>
2011-03-17 03:42:17 -0300

    Testing sendgrid post

--- app.rb
+++ app.rb
@@ -13,6 +13,17 @@ module SendgridEventProxy
       SendgridEvent.create(data) rescue nil
       nil
     end
+    
+    post /.*/ do
+      request.body.rewind
+      data = JSON.parse(request.body.read)
+      # para teste apenas
+      
+      File.open('teste_2.yml','w') do |f|
+        f.puts(request.to_yaml)
+      end
+      
+    end
 
     # para testar:
     # curl -H 'Content-Type: application/json' -d "{\"event\":\"processed\",\"email\":\"example@example.org\"}" -X POST http://localhost:4567/sendgrid_event.json
