81b6de9cdc0753106d780f97ff13fd806e795998
thiagobrandam <thiago@thiago.redealumni>
2011-03-17 12:56:52 -0300

    Changed code so sendgrid works: they send post data trhough ampersand-separated values

--- app.rb
+++ app.rb
@@ -1,31 +1,16 @@
 module SendgridEventProxy
   class Application < Sinatra::Base
     
-    post "/sendgrid_event.json" do
-      request.body.rewind
-      data = JSON.parse(request.body.read)
-      # para teste apenas
-      
+    post "/sendgrid_event" do
       File.open('teste.yml','w') do |f|
-        f.puts(data.to_yaml)
+        f.puts(params.to_yaml)
       end
       
-      SendgridEvent.create(data) rescue nil
+      SendgridEvent.create(params) rescue nil
       nil
     end
     
-    post /.*/ do
-      request.body.rewind
-      data = JSON.parse(request.body.read)
-      # para teste apenas
-      
-      File.open('teste_2.yml','w') do |f|
-        f.puts(request.to_yaml)
-      end
-      
-    end
-
     # para testar:
-    # curl -H 'Content-Type: application/json' -d "{\"event\":\"processed\",\"email\":\"example@example.org\"}" -X POST http://localhost:4567/sendgrid_event.json
+    # curl -D - -d "event=processed&email=test" http://localhost:4567/sendgrid_event    
   end
 end
